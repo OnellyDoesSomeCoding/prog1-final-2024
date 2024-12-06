126218fb492ade5d80992ce85bf441cb1e17e3d3
Khittitaj BUNUPURADAH <jellyhill.138@gmail.com>
2024-12-06 12:00:20 +0700

    Making run_ball.py my 'main part'

--- run_ball.py
+++ run_ball.py
@@ -1,50 +1,31 @@
 import turtle
 import ball
-import random
+from seven_segments_proc import  Segment
 
-num_balls = 5
 turtle.speed(0)
 turtle.tracer(0)
 turtle.hideturtle()
-canvas_width = turtle.screensize()[0]
-canvas_height = turtle.screensize()[1]
-print(canvas_width, canvas_height)
-ball_radius = 0.05 * canvas_width
+print(ball.Run().canvas_width, ball.Run().canvas_height)
+ball_radius = 0.05 * ball.Run().canvas_width
 turtle.colormode(255)
-xpos = []
-ypos = []
-vx = []
-vy = []
-ball_color = []
-
-# create random number of balls, num_balls, located at random positions within the canvas; each ball has a random velocity value in the x and y direction and is painted with a random color
-for i in range(num_balls):
-    xpos.append(random.uniform(-1*canvas_width + ball_radius, canvas_width - ball_radius))
-    ypos.append(random.uniform(-1*canvas_height + ball_radius, canvas_height - ball_radius))
-    vx.append(10*random.uniform(-1.0, 1.0))
-    vy.append(10*random.uniform(-1.0, 1.0))
-    ball_color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-
-def draw_border():
-    turtle.penup()
-    turtle.goto(-canvas_width, -canvas_height)
-    turtle.pensize(10)
-    turtle.pendown()
-    turtle.color((0, 0, 0))
-    for i in range(2):
-        turtle.forward(2*canvas_width)
-        turtle.left(90)
-        turtle.forward(2*canvas_height)
-        turtle.left(90)
 
 dt = 0.2 # time step
+b1 = ball.Run()
+b1.create_random()
 while (True):
     turtle.clear()
-    draw_border()
-    for i in range(num_balls):
-        ball.draw_ball(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_ball(i, xpos, ypos, vx, vy, dt)
-        ball.update_ball_velocity(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius)
+    b1.draw_border()
+    Tom = turtle.Turtle()
+    tom_color = (255, 0, 0)
+    sgm = Segment(Tom, tom_color)
+    # sgm.clear(Tom)
+    for j in range(8):
+        sgm.draw(j)
+    for i in range(ball.Run().num_balls):
+        bb = ball.Ball(b1.ball_color[i], b1.ball_radius, b1.xpos[i], b1.ypos[i])
+        bb.draw_ball()
+        bb.move_ball(i, dt, b1)
+        bb.update_ball_velocity(i, b1)
     turtle.update()
 
 # hold the window; close it by clicking the window close 'x' mark
